a4437d764c4cccd433fe045c214385b08746a897
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-06 19:52:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ function destructivelyAppendKitten(name){kittens.push(name); return kittens}
 function destructivelyPrependKitten(name){kittens.unshift(name); return kittens}
 function destructivelyRemoveLastKitten(){return kittens.pop()}
 function destructivelyRemoveFirstKitten(){return kittens.shift()}
+function appendKitten(name){return 
