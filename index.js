6cf3ab49b00a55337441acaa8d86592fb074e7be
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-06 19:33:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(kittens, element){kittens.appendKitten(); return kittens}
+function destructivelyAppendKitten(kittens){kittens.appendKitten(kittens); return kittens}
