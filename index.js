e474015a7030fc5f5b8fbb0bdb0640c8c830c01d
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-06 19:34:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(kittens){kittens.appendKitten(kittens); return kittens}
+function destructivelyAppendKitten(kittens){appendKitten(kittens); return kittens}
